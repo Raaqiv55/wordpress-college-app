9e745ed1f172c9103fa397a9c5178f8f8885ada6
alamgir hossain <alamgir555hossain@gmail.com>
2019-05-17 01:11:01 -0400

    initial commit

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -5,7 +5,7 @@
       <div class="group">
 
         <div class="site-footer__col-one">
-          <h1 class="school-logo-text school-logo-text--alt-color"><a href="#"><strong>Fictional</strong> University</a></h1>
+          <h1 class="school-logo-text school-logo-text--alt-color"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
           <p><a class="site-footer__link" href="#">555.555.5555</a></p>
         </div>
 
@@ -14,11 +14,11 @@
             <h3 class="headline headline--small">Explore</h3>
             <nav class="nav-list">
               <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
-              </ul>
+               <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+               <li><a href="#">Programs</a></li>
+               <li><a href="#">Events</a></li>
+               <li><a href="#">Campuses</a></li>
+             </ul> 
             </nav>
           </div>
 
@@ -26,10 +26,10 @@
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
               <ul>
-                <li><a href="#">Legal</a></li>
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Careers</a></li>
-              </ul>
+               <li><a href="#">Legal</a></li>
+               <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
+               <li><a href="#">Careers</a></li>
+             </ul> 
             </nav>
           </div>
         </div>
--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-sclae=1">
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="#"><strong>Fictional</strong> University</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-          <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Programs</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
-          </ul>
+          
+      <ul>
+          <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 5) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+          <li><a href="#">Programs</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Campuses</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
